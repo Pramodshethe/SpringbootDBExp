895ddd2c21cefb764ea05f04b66f9d91c649e9a5
Pramodkumar <30823288+Pramodshethe@users.noreply.github.com>
2018-02-21 19:07:22 +0530

    Add files via upload

---
+++ ContactAppUsingSpringBootJPA/src/main/java/com/pramod/shethe/config/MyContactAppInitilizer.java
@@ -0,0 +1,32 @@
+package com.pramod.shethe.config;
+
+import org.springframework.web.SpringServletContainerInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class MyContactAppInitilizer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        WebApplicationContext context = getContext();
+        servletContext.addListener(new ContextLoaderListener(context));
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("DispatcherServlet",
+                new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("*.html");
+    }
+
+    private AnnotationConfigWebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("com.pramod.shethe.config.WebapplicationConfiguration");
+        return context;
+    }
+
+}
---
+++ ContactAppUsingSpringBootJPA/src/main/java/com/pramod/shethe/config/WebapplicationConfiguration.java
@@ -0,0 +1,48 @@
+package com.pramod.shethe.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.Filter;
+import java.io.IOException;
+
+@Configuration
+@ComponentScan(basePackages = "com.pramod.shethe")
+@EnableJpaRepositories("com.pramod.shethe.repositories")
+@EnableWebMvc
+@ConfigurationProperties(value = "classpath:/application.yml")
+public class WebapplicationConfiguration {
+
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        return resolver;
+    }
+
+
+    @Bean
+    public PropertySource yamlPropertySourceLoader() throws IOException {
+        YamlPropertySourceLoader yamlPropertySourceLoader = new YamlPropertySourceLoader();
+        return (PropertySource) yamlPropertySourceLoader.load("application.yml",
+                new ClassPathResource("application.yml"), "default");
+    }
+
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
